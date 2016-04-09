ca2b8c4e5858a577c805bb8e1f99c3a0ece45913
tfs1 <migreene@outlook.com>
2016-04-09 16:33:43 -0500

    first pass at task plugin

---
+++ PSDeploy/PSDeployScripts/Task.ps1
@@ -0,0 +1,25 @@
+<#
+    .SYNOPSIS
+        Execute scripts described in psdeploy files
+
+    .DESCRIPTION
+        Support deployments by handling simple tasks.
+
+    .PARAMETER Deployment
+        Name for the work that will be performed.
+#>
+[cmdletbinding()]
+param (
+    [ValidateScript({ $_.PSObject.TypeNames[0] -eq 'PSDeploy.Deployment' })]
+    [psobject[]]$Deployment
+)
+
+Write-Verbose "Executing $($Deployment.count) tasks"
+
+foreach($task in $Deployment)
+{
+    if($task.SourceExists)
+    {
+        PowerShell.exe -noninteractive -executionpolicy remotesigned -file "$($task.Source)"
+    }
+}
---
+++ Tests/artifacts/DeploymentsTask.psdeploy.ps1
@@ -0,0 +1,5 @@
+Deploy Example {
+    By Task {
+        FromSource .\Tasks\createfile.ps1
+    }
+}
