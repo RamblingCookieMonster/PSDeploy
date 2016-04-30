e9fdc159265402ce710812ce04272bbb3d88f078
Brandon Olin <brandon@devblackops.io>
2016-04-29 21:44:05 -0700

    Add test deployment script for PSGalleryModule

---
+++ Tests/artifacts/DeploymentPSGalleryModule.psdeploy.ps1
@@ -0,0 +1,10 @@
+Deploy TestModule {
+    By PSGalleryModule TestModule {
+        FromSource '\Modules\TestModule'
+        To 'PSGallery'
+        Tagged Testing
+        WithOptions @{
+            ApiKey = '0c3e374b-49a3-4b05-a597-fd45773a4fb6'
+        }
+    }
+}
