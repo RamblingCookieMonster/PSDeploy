bb79bd73f34d5f8e5e04f30f15405997d8c7d3eb
Brandon Olin <brandon@devblackops.io>
2016-05-10 20:11:18 -0700

    Remove support for specifying just module name

--- PSDeploy/PSDeployScripts/PSGalleryModule.ps1
+++ PSDeploy/PSDeployScripts/PSGalleryModule.ps1
@@ -4,7 +4,7 @@
 
     .DESCRIPTION
         Deploys a module to a PowerShell-based repository like the PowerShell Gallery. Can also be used to deploy to a private
-        instance of PowerShell Gallery.
+        instance of PowerShell Gallery or to any nuget-based repository.
 
     .PARAMETER Deployment
         Deployment to run
@@ -21,34 +21,28 @@ param(
     [string]$ApiKey
 )
 
-foreach($Deploy in $Deployment) {
-    foreach($target in $Deploy.Targets) {
-        Write-Verbose -Message "Starting deployment [$($Deploy.DeploymentName)] to PowerShell reposotory [$Target]"
+foreach($deploy in $Deployment) {
+    foreach($target in $deploy.Targets) {
+        Write-Verbose -Message "Starting deployment [$($deploy.DeploymentName)] to PowerShell repository [$Target]"
         
         # Validate that $target has been setup as a valid PowerShell repository
-        $validRepo = Get-PSRepository -Name $target -ErrorAction SilentlyContinue
+        $validRepo = Get-PSRepository -Name $target -Verbose:$false -ErrorAction SilentlyContinue
         if (-not $validRepo) {
-            throw "$target has not been setup as a valid PowerShell repository."
+            throw "[$target] has not been setup as a valid PowerShell repository."
         }
 
-        # Publish-Module params
+        # Publish-Module supports specifying either the name of a module or the path to a module.        
+        # Since PSDeploy validates that the value specified in 'FromSource' is a valid path before
+        # invoking the deployment script, we don't support specifying just the module name as that
+        # would cause PSDeploy to throw an error when validating the value in FromSouce. For this 
+        # deployment script, only the path to the module root is supported.
         $params = @{
+            Path = $deploy.Source
             Repository = $target
-            NuGetApiKey = $Deploy.DeploymentOptions.ApiKey
+            NuGetApiKey =  $deploy.DeploymentOptions.ApiKey
             Verbose = $VerbosePreference
-            WhatIf = $true
         }
-        
-        # The source could be a path to a module or just the name of a module.
-        # In the case of a name, Publish-Module will search for the module in $env:PSModulePath
-        # and will use the first one it finds. This may not me what you want. You may be better
-        # off specifying an explicit path or combine the module name with a version number.
-        if (Test-Path -Path $Deploy.Source) {
-            $params.Path = $Deploy.Source
-        } else {
-            $params.Name = $Deploy.Source
-        }
-        
+                
         Publish-Module @params
     }
 }
