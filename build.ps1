604d6d82b7eb2c00869a595bade954b7abe319a8
Brandon Padgett <gerane@gmail.com>
2016-06-15 16:23:39 -0500

    Added Resolve-Module to build.ps1
    * Added Resolve-Module to build.ps1 to make local deployments easier

--- build.ps1
+++ build.ps1
@@ -1,8 +1,54 @@
-﻿# Grab nuget bits, install modules, set build variables, start build.
+﻿function Resolve-Module
+{
+    [Cmdletbinding()]
+    param
+    (
+        [Parameter(Mandatory)]
+        [string[]]$Name
+    )
+
+    Process
+    {
+        foreach ($ModuleName in $Name)
+        {
+            $Module = Get-Module -Name $ModuleName -ListAvailable
+            Write-Verbose -Message "Resolving Module $($Module.Name[0])"
+            
+            if ($Module) 
+            {                
+                $Version = $Module | Measure-Object -Property Version -Maximum | Select-Object -ExpandProperty Maximum
+                $GalleryVersion = Find-Module -Name $ModuleName -Repository PSGallery | Measure-Object -Property Version -Maximum | Select-Object -ExpandProperty Maximum
+
+                if ($Version -lt $GalleryVersion)
+                {
+                    
+                    if ((Get-PSRepository -Name PSGallery).InstallationPolicy -ne 'Trusted') { Set-PSRepository -Name PSGallery -InstallationPolicy Trusted }
+                    
+                    Write-Verbose -Message "$($Module.Name[0]) Installed Version [$($Version.tostring())] is outdated. Installing Gallery Version [$($GalleryVersion.tostring())]"
+                    
+                    Install-Module -Name $ModuleName -Force
+                    Import-Module -Name $ModuleName -Force -RequiredVersion $GalleryVersion
+                }
+                else
+                {
+                    Write-Verbose -Message "Module Installed, Importing $($Module.Name[0])"
+                    Import-Module -Name $ModuleName -Force -RequiredVersion $Version
+                }
+            }
+            else
+            {
+                Write-Verbose -Message "$($Module.Name[0]) Missing, installing Module"
+                Install-Module -Name $ModuleName -Force
+                Import-Module -Name $ModuleName -Force -RequiredVersion $Version
+            }
+        }
+    }
+}
+
+# Grab nuget bits, install modules, set build variables, start build.
 Get-PackageProvider -Name NuGet -ForceBootstrap | Out-Null
 
-Install-Module Psake, PSDeploy, Pester, BuildHelpers -force
-Import-Module Psake, BuildHelpers
+Resolve-Module Psake, PSDeploy, Pester, BuildHelpers -force
 
 Set-BuildEnvironment
 
