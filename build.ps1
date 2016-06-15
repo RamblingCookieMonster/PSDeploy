05657899c09fba3d484f743244d67eabf148fb67
Brandon Padgett <gerane@gmail.com>
2016-06-15 16:34:37 -0500

    Removed -Force from Resolve-Module

--- build.ps1
+++ build.ps1
@@ -48,7 +48,7 @@
 # Grab nuget bits, install modules, set build variables, start build.
 Get-PackageProvider -Name NuGet -ForceBootstrap | Out-Null
 
-Resolve-Module Psake, PSDeploy, Pester, BuildHelpers -force
+Resolve-Module Psake, PSDeploy, Pester, BuildHelpers
 
 Set-BuildEnvironment
 
