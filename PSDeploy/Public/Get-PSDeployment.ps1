949fcc4addec081adc2914a8283688fd573f63f9
Brandon Padgett <gerane@gmail.com>
2016-06-16 15:28:19 -0500

    Resolve ProviderPath to Handle PSDrive and UNC

--- PSDeploy/Public/Get-PSDeployment.ps1
+++ PSDeploy/Public/Get-PSDeployment.ps1
@@ -162,7 +162,7 @@
                     #Fail silently if not file/folder...
                     if(Test-Path $Source -ErrorAction SilentlyContinue)
                     {
-                        $LocalSource = ( Resolve-Path $Source ).Path
+                        $LocalSource = ( Resolve-Path $Source ).ProviderPath
                     }
                     else
                     {
