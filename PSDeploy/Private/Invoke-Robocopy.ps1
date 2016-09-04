4dd2adb8f82b33891e2d93eea6098eaf2d6514bc
FooBartn <barton.joshua@gmail.com>
2016-09-04 15:10:25 -0500

    Added EnableExit param and removed trailing backslashes from paths

--- PSDeploy/Private/Invoke-Robocopy.ps1
+++ PSDeploy/Private/Invoke-Robocopy.ps1
@@ -11,21 +11,37 @@ function Invoke-Robocopy () {
         $Destination,
 
         # List of arguments to be splatted to ROBOCOPY
-        [Parameter(Mandatory=$true)]
+        [Parameter(Mandatory=$false)]
         [array]
-        $ArgumentList
+        $ArgumentList,
+        
+        # Exit function if robocode throws "terminating" error code
+        [Parameter(Mandatory=$false)]
+        [switch]
+        $EnableExit
     )
+
+    # Remove trailing backslash
+    $Path = $Path -replace '\\$'
+    $Destination = $Destination -replace '\\$'
 
     ROBOCOPY.exe $Path $Destination @ArgumentList
 
     switch ($LastExitCode) {
         0   { Write-Output 'No files copied. Source and destination are in sync' }
         1   { Write-Output 'Files were copied successfully'}
-        2   { Write-Output 'Extra files/directories detected. Housekeeping may be required.'}
-        4   { Write-Output 'Mismatched files/directories detected. Housekeeping may be required.'}
-        8   { Write-Error 'Some files/directories could not be copied'}
-        10  { Write-Error 'Usage error or an error due to insufficient access privileges'}
+        2   { Write-Warning 'Extra files/directories detected. Housekeeping may be required.'}
+        4   { Write-Warning 'Mismatched files/directories detected. Housekeeping may be required.'}
+        8   { $RCError = 'Some files/directories could not be copied'}
+        10  { $RCError = 'Usage error or an error due to insufficient access privileges'}
+        16  { $RCError = 'No destination directory specified'}
     }
 
-    exit ($LastExitCode -band 24)
+    if ($LastExitCode -gt 4) {
+        if ($EnableExit) {
+            $host.SetShouldExit($LastExitCode)
+        } else {
+            Write-Error $RCError
+        }
+    }
 }
