8b9eeda174cc5a88c508dc40d872be5268add3f1
Warren Frame <ramblingcookiemonster@users.noreply.github.com>
2017-01-22 15:05:05 -0500

    Add commas to array declaration

--- PSDeploy/PSDeployScripts/FilesystemRemote.ps1
+++ PSDeploy/PSDeployScripts/FilesystemRemote.ps1
@@ -64,8 +64,8 @@ Catch
 
 # Set up module functions that need to be injected into the remote session.
 $FunctionsToInject = @(
-    'Get-Hash'
-    'Invoke-Robocopy'
+    'Get-Hash',
+    'Invoke-Robocopy',
     'Start-ConsoleProcess'
 )
 $InjectedFunctions = @()
