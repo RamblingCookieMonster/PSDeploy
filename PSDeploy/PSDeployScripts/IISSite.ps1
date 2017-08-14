75134199e967212c974b5dd6b99e38cd6f48a3ea
Markus Mattes <M.Mattes@vetz.de>
2017-08-14 23:56:17 +0200

    Finished the IISSite deployment and added a new type for deploying an IISAppPool

--- PSDeploy/PSDeployScripts/IISSite.ps1
+++ PSDeploy/PSDeployScripts/IISSite.ps1
@@ -1,34 +1,49 @@
 <#
     .SYNOPSIS
-        Support deployments by handling simple tasks.
+        Deploys an IISSite using Powershell
 
     .DESCRIPTION
-        Support deployments by handling simple tasks.
-
-        You can use a Task in two ways:
-
-        As a scriptblock:
-
-            By Task {
-                "Run some deployment code in this scriptblock!"
+        Deploys an IISSite using Powershell
+    
+        By IISSite MyWebsite {
+            To .\wwwroot
+            WithOptions @{
+                name = "SiteName"
+                bindings = @(
+                    @{protocol="http"; bindingInformation="*:80:www.myurl.com" }
+                    @{protocol="https"; bindingInformation="*:443:www.myurl.com" }
+                    )
             }
+        }
 
-        As a script:
-
-            By Task {
-                FromSource "Path\To\SomeDeploymentScript.ps1"
-            }
+        To: Is the root path of the Website to deploy using IIS, currently only one target is supported
 
     .PARAMETER Deployment
         Deployment to process
+
+    .PARAMETER Name
+        Name of the Site under which it will be listed in IIS
+
+    .PARAMETER AppPool
+        AppPool to be used for this site in IIS, will be created if it does not exist. But it is recommend to use the extra 
+        Deployment Type IISAppPool to setup a AppPool with specific settings.
+
+    .PARAMETER Bindings
+        Bindings under which this Site should be reachable
 #>
 [cmdletbinding()]
 param (
     [ValidateScript({ $_.PSObject.TypeNames[0] -eq 'PSDeploy.Deployment' })]
     [psobject[]]$Deployment,
-    [string]  $name     = $null,    
-    [string]  $appPool  = "DefaultAppPool",
-    [Object[]]$bindings = $null
+
+    [Parameter(Mandatory=$true, Position=1)]
+    [string]  $Name     = $null,    
+
+    [Parameter(Mandatory=$false, Position=2)]
+    [string]  $AppPool  = "DefaultAppPool",
+
+    [Parameter(Mandatory=$false, Position=3)]
+    [Object[]]$Bindings = $null
 )
 
 <#
@@ -37,21 +52,18 @@ param (
 function Create-IISWebsite
 {
     param(
+        [Parameter(Mandatory=$true, Position=1)]
         [string]  $name     = $null,
+
+        [Parameter(Mandatory=$true, Position=2)]
         [string]  $root     = $null,
+
         [string]  $appPool  = "DefaultAppPool",
         [Object[]]$bindings = $null
     )
     
     begin
     {
-    
-        if(-not $name) { throw "Empty site name, Argument -Name is missing"        }
-        if(-not $root) { throw "Empty website root path, Argument -Root is missing"}
-        
-        
-        # SETTINGS
-        
         # IIS Directory Settings
         [string]$iisSite = "IIS:\Sites\$($name)\"
     }
@@ -81,20 +93,16 @@ function Create-IISWebsite
 #>
 function Remove-IISWebsite
 {
-    param(        
-        # Website Settings
+    param(                
+        [Parameter(Mandatory=$true, Position=1)]
         [string]$name    = $null,
+
+        [Parameter(Mandatory=$true, Position=2)]
         [string]$root    = $null
     )
     
     begin
     {
-        if(-not $name) { throw "Empty site name, Argument -Name is missing"        }
-        if(-not $root) { throw "Empty website root path, Argument -Root is missing"}
-        
-        
-        # SETTINGS
-        
         # IIS Directory Settings
         [string]$iisSite = "IIS:\Sites\$($name)"
     }
@@ -122,9 +130,18 @@ foreach($site in $Deployment)
         Import-Module WebAdministration -ErrorAction Stop
         Sleep 2 #see http://stackoverflow.com/questions/14862854/powershell-command-get-childitem-iis-sites-causes-an-error
     }
-    # TODO: Multiple Targets
-    # TODO: Remote Machines
+
+    [string]$iisPoolPath = "IIS:\AppPools\$($AppPool)\"
+
+    if(!(Test-Path $iisPoolPath))
+    {
+        Write-Verbose "AppPool $name does not exist, it will be created with default settings. For special settings please use IISAppPool as individual Deployment Type"
+        # Workaround to call the IISAppPool creation from within this Deployment type $Deployment is forwarded but not processed.
+        . $PSScriptRoot\IISAppPool.ps1 -Deployment $Deployment -Name $AppPool
+    }
+
     $root = $site.Targets[0]
-    Remove-IISWebsite -name $name -root $root
-    Create-IISWebsite -name $name -root $root -bindings $bindings
+
+    Remove-IISWebsite -name $Name -root $root
+    Create-IISWebsite -name $Name -root $root -bindings $Bindings -appPool $AppPool
 }
