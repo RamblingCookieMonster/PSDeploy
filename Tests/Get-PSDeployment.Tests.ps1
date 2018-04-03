1975f69268178885a8ebd3a2a3de0004dbdb86c0
Juan Perez <juanfperezperez@hotmail.com>
2018-04-03 09:30:40 -0400

    use System.IO.Path.Combine to cunstruct paths valid in windows and linux

--- Tests/Get-PSDeployment.Tests.ps1
+++ Tests/Get-PSDeployment.Tests.ps1
@@ -138,7 +138,7 @@ InModuleScope 'PSDeploy' {
         }
 
         Context 'Should allow user-specified options from ps1' {
-            $Deployments = @( Get-PSDeployment @Verbose -Path $ProjectRoot\Tests\artifacts\DeploymentsRaw.PSDeploy.ps1 )
+            $Deployments = @( Get-PSDeployment @Verbose -Path  ([System.IO.Path]::Combine([string[]]@($ProjectRoot,'Tests','artifacts','DeploymentsRaw.psdeploy.ps1'))) )
 
             It 'Should have expected Count' {
                 $Deployments.Count | Should Be 1
